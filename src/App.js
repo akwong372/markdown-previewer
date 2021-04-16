f39d69ab856e5b303432fd22816cff1b44379601
akwong372 <akwong372@gmail.com>
2021-04-16 15:54:33 -0700

    adds default markdown in textarea

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import DOMPurify from 'dompurify';
 
 const App = () => {
 
-  const [text, setText] = useState('');
+  const defaultText = `# Heading
+  ## Subheading
+  [Inline link](https://www.google.com)\n
+  \`inline code\`\n
+    code block\n
+  1. First ordered list item
+  2. Another item
+
+  >blockquote\n
+
+  **bolded text**
+
+  image\n
+  ![alt text](https://i.imgur.com/YNqda8M.jpg "meme")`;
+  const [text, setText] = useState(defaultText);
 
   return (
     <div className='container'>
 
-      <textarea id='editor' placeholder='Markdown here' onChange={e=>setText(DOMPurify.sanitize(marked(e.target.value)))}></textarea>
+      <textarea id='editor' placeholder='Markdown here' onChange={e=>setText(DOMPurify.sanitize(e.target.value))} defaultValue={defaultText}>
+      </textarea>
 
-      <div id='preview' dangerouslySetInnerHTML={{__html: text}}>
+      <div id='preview' dangerouslySetInnerHTML={{__html: marked(text)}}>
         </div>
 
     </div>
